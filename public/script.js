f67813fe2284c7b8fe6f9b5ed5eb59c45ef318f4
msmichellegar <msmichellegar@gmail.com>
2016-02-22 16:58:15 +0000

    adds content hide functionality

--- public/script.js
+++ public/script.js
@@ -1,25 +1,43 @@
 $(document).ready(function() {
+    var instagramToken = window.location.pathname.split('/')[2];
 
-    $.ajax({
-        url: "/instagram",
-        complete: function(data) {
-            console.log(data);
-        }
-    });
+    // $.ajax({
+    //     url: "https://api.instagram.com/v1/users/1655822653/media/recent/?count=5?access_token=" + instagramToken,
+    //     complete: function(data) {
+    //         console.log(data);
+    //     }
+    // });
 
     $.ajax({
         url: "/twitter/msmichellegar",
         complete: function(data) {
             parseTweets(data);
+            enableContentHiding()
         }
     });
 
 });
 
 function parseTweets(data) {
+
     $("#news").html("");
     for (var i=0; i<data.responseJSON.length; i++) {
-        $("#news").append("<p>" + data.responseJSON[i].created_at + " " + data.responseJSON[i].text + "</p>");
+
+        if (data.responseJSON[i].extended_entities) {
+
+            $("#news").append("<div class='update'><p class='close'>x</p><p>" + data.responseJSON[i].created_at + "</p><p>" + data.responseJSON[i].text + "</p><img src='" + data.responseJSON[i].extended_entities.media[0].media_url + "'/></div>");
+
+        } else {
+            $("#news").append("<div class='update'><p class='close'>x</p><p>" + data.responseJSON[i].created_at + "</p><p>" + data.responseJSON[i].text + "</p></div>");
+        }
     }
 
+}
+
+function enableContentHiding() {
+
+    $(".close").click(function() {
+        // console.log();
+        $(this.parentNode).fadeOut();
+    });
 }
