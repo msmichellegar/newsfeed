324550bb8f0bf53e9589f7d69ad3423593602c2c
msmichellegar <msmichellegar@gmail.com>
2016-02-22 17:09:13 +0000

    sends envs

--- public/script.js
+++ public/script.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
     $.ajax({
         url: "/twitter/msmichellegar",
         complete: function(data) {
-            console.log("data", data);
             parseTweets(data);
             enableContentHiding();
         }
